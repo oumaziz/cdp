3767cd9924addb59f0840b0eb26b4fab44ac805e
Yedir <yedir.ouarmassi@laposte.net>
2015-10-23 00:43:33 +0200

    [US3][IMPBAK][Doing]{techniquement fini , mais le sql ne trouve pas Userstory}

--- app/Http/Controllers/BacklogController.php
+++ app/Http/Controllers/BacklogController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Userstory;
 
 class BacklogController extends Controller
 {
     public function show(){
-        $userstories=[];
+        $userstories= Userstory::all();
+        return $userstories;
         return view("Backlog")->with('userstories',$userstories);
     }
 }

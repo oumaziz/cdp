4d223ffc80331caea88c611129d132296f1fbc6b
Yedir <yedir.ouarmassi@laposte.net>
2015-10-23 22:17:37 +0200

    [US6][PAG-US[Done]{graphist needed but seems functional}

--- app/Http/Controllers/UsController.php
+++ app/Http/Controllers/UsController.php
@@ -9,7 +9,9 @@ use DB;
 
 class UsController extends Controller
 {
-
+    public function create(){
+        return view("UsForm")->with('victim',[]);
+    }
     public function modify($idUs){
 
         $victim =DB::table('userstory')->where('id', '=', $idUs)->get() ;

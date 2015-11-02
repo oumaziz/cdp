1271d7225e3c05d6056ee710326d947c58d6c7a6
Mohamed Oumaziz <mohamed@oumaziz.com>
2015-11-02 19:52:49 +0100

    [US#4][IMP-US][Done] Fonctionnelle

--- app/Http/Controllers/UsController.php
+++ app/Http/Controllers/UsController.php
@@ -2,19 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Userstory;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Redirect;
 use DB;
 
 class UsController extends Controller
 {
     public function create(){
-        return view("UsForm")->with('victim',[]);
+        return view("UsForm")->with('us',[]);
     }
     public function modify($idUs){
 
-        $victim =DB::table('userstory')->where('id', '=', $idUs)->get() ;
-        return view("UsForm")->with('victim',head($victim));
+        $us =DB::table('userstory')->where('id', '=', $idUs)->get() ;
+        return view("UsForm")->with('us',head($us));
+    }
+
+    public function createConfirm(Request $r){
+
+        $developer_id = 1;
+        $project_id = DB::table('project')->where('developer_id', $developer_id)->first()->id;
+
+        Userstory::create([
+            "description" => $r->input('description'),
+            "priority" => $r->input('priority'),
+            "difficulty" => $r->input('difficulty'),
+            "status" => 0,
+            "project_id" => $project_id]);
+
+        return Redirect::action("BacklogController@show");
+    }
+    public function modifyConfirm(Request $r){
+
+
+
+        return Redirect::action("BacklogController@show");
     }
 }
--- resources/views/UsForm.blade.php
+++ resources/views/UsForm.blade.php
@@ -3,28 +3,31 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<title>Ajouter/Modifier une UserStory</title>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <title>Ajouter/Modifier une UserStory</title>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 </head>
 <body>
-@if($victim)
+@if($us)
+    <form action="{{  URL::action("UsController@modifyConfirm", null) }}" class="form-horizontal" method="POST">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="us" value="{{ $us->id }}">
 
-<p>
-	<label for="description">Description</label> : <input type ="text" name="description" id="description" size="100" maxlength="500" value ="<?php echo $victim->description; ?>"/> <br />
-	<label for="priority">Priorité</label> : <input type ="number" name="priority" id="priority" min="0" value="<?php echo $victim->priority; ?>" /> <br />
-	<label for="difficulty">Difficulté</label> : <input type ="number" name="difficulty" id="difficulty" min="0" value="<?php echo $victim->difficulty; ?>" /> <br />
-	<input type ="checkbox" name="done" id="done" value="<?php echo $victim->status; ?>" /><label for="done">Terminée</label> <br />
-
-	</form>
-</p>
+        <label for="description">Description</label> : <input type ="text" name="description" id="description" size="100" maxlength="500" value ="<?php echo $us->description; ?>"/> <br />
+        <label for="priority">Priorité</label> : <input type ="number" name="priority" id="priority" min="0" value="<?php echo $us->priority; ?>" /> <br />
+        <label for="difficulty">Difficulté</label> : <input type ="number" name="difficulty" id="difficulty" min="0" value="<?php echo $us->difficulty; ?>" /> <br />
+        <input type ="checkbox" name="done" id="done" value="done" <?php if($us->status == 1) echo 'checked="checked"' ?> /><label for="done">Terminée</label> <br />
+        <button type="submit" class="btn">Ajouter</button>
+    </form>
 @else
-	<label for="description">Description</label> : <input type ="text" name="description" id="description" size="100" maxlength="500" /> <br />
-	<label for="priority">Priorité</label> : <input type ="number" name="priority" id="priority" min="0"  /> <br />
-	<label for="difficulty">Difficulté</label> : <input type ="number" name="difficulty" id="difficulty" min="0"  /> <br />
-	<input type ="checkbox" name="done" id="done"  /><label for="done">Terminée</label> <br />
+    <form action="{{  URL::action("UsController@createConfirm", null) }}" class="form-horizontal" method="POST">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-	</form>
+        <label for="description">Description</label> : <input type ="text" name="description" id="description" size="100" maxlength="500" /> <br />
+        <label for="priority">Priorité</label> : <input type ="number" name="priority" id="priority" min="0"  /> <br />
+        <label for="difficulty">Difficulté</label> : <input type ="number" name="difficulty" id="difficulty" min="0"  /> <br />
+        <button type="submit" class="btn">Ajouter</button>
+    </form>
 @endif
 </body>
 </html>

23b458977731f761e767e266dde3514c47be42d7
nasserdone <nassertijani@gmail.com>
2015-11-04 20:35:58 +0100

    [US#6][MODEL][DONE]

--- app/Tache.php
+++ app/Tache.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tache extends Model
 {
+
+
+    protected $table = 'tache';
     protected $fillable = array('description','code','start_date','end_date','us_story_id','predecessors');
+
+
+
+
+
 
 }

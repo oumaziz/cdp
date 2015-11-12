9e85c00cf887df7a23702322cb6da80c4d2c1dc5
Mohamed Oumaziz <mohamed@oumaziz.com>
2015-11-12 18:45:09 +0100

    [US#15][IMPVHC][Done] Tâche terminée

--- app/Http/Controllers/CommitsController.php
+++ app/Http/Controllers/CommitsController.php
@@ -13,6 +13,11 @@ class CommitsController extends Controller
 
     public function show($project_id, $key = null) {
 
-        
+        if($key != null){
+            if(Visitor::where("key", $key)->where("project_id", $project_id)->get() != null)
+                return "Accès autorisé pour le visiteur, on affiche la page";
+        }
+
+        return Redirect()->action('Auth\AuthController@getLogin');
     }
 }

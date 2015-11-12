0159e172b7bc829d8ad652722729961c8c7ea39e
Mohamed Oumaziz <mohamed@oumaziz.com>
2015-11-12 18:44:27 +0100

    [US#15][IMPVSP][Done] Tâche terminée

---
+++ app/Http/Controllers/CommitsController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: oumaziz
+ * Date: 12/11/15
+ * Time: 11:32
+ */
+
+namespace App\Http\Controllers;
+use App\Visitor;
+
+class CommitsController extends Controller
+{
+
+    public function show($project_id, $key = null) {
+
+        
+    }
+}

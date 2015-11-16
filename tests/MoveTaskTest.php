6df99ffc7a4f025cd958b4db649afb708d43f3db
Mohamed Oumaziz <mohamed@oumaziz.com>
2015-11-16 19:08:04 +0100

    [US#17][TU-DTA][Done]

--- tests/MoveTaskTest.php
+++ tests/MoveTaskTest.php
@@ -14,7 +14,22 @@ class MoveTaskTest extends TestCase
      */
     public function testMoveTask()
     {
+        $tache = Tache::create([
+            'description'=>'Ma tache',
+            'code' => "MoncodeTest",
+            'start_date' =>'2015-11-15',
+            'end_date'   =>'2015-11-30',
+            'us_story_id' => '1'
+        ]);
 
+        $take = new TakeTacheController();
+        $take->edit($tache->id);
+
+        $tacheUpdated = Tache::where("code", "MoncodeTest")->get()->first();
+
+        $this->assertEquals(1, $tacheUpdated->state);
+
+        $tacheUpdated->delete();
     }
 }
 
